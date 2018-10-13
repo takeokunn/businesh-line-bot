18cf221320e63db7da4ac74ff343d30603457f6a
takeokunn <bararararatty@gmail.com>
2018-10-14 08:53:54 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const businesh = require('businesh');
+const line = require('@line/bot-sdk');
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+};
+
+const app = express();
+const client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+    const events = req.body.events;
+    events.map(event => handle_event(event));
+});
+
+const handle_event = event => {
+    if (event.type !== 'message' || event.message.type !== 'text') return;
+    businesh.translate(event.message.text)
+        .then(after_text => client.replyMessage(event.replyToken, { type: 'text', text: after_text }));
+        .catch(err => console.log(err));
+};
+
+const port = process.env.PORT || 3000;
+app.listen(port);
